dc1bd85188e2b93ddf26a216c5c70de52c6852a6
Mikhail Zinin <mzinin@gmail.com>
2019-07-03 11:30:19 +0200

    Evaluator unit tests refactored and extended

--- src/test/java/mzinin/petproject/s2e2/EvaluatorTests.java
+++ src/test/java/mzinin/petproject/s2e2/EvaluatorTests.java
@@ -5,30 +5,97 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 
 class EvaluatorTests {
 
     @Test
-    void noFunctionsNoOperatorsEmptyFunctions() {
+    void positiveTest_AddFunction_VerifyTokenizer() {
+        final ITokenizer tokenizerMock = mock(ITokenizer.class);
+        final Evaluator evaluator = new Evaluator(null, tokenizerMock);
+        final Function dummyFunction = makeDummyFunction();
+
+        evaluator.addFunction(dummyFunction);
+
+        verify(tokenizerMock).addFunction(dummyFunction.name);
+    }
+
+    @Test
+    void positiveTest_AddOperator_VerifyConverter() {
+        final IConverter converterMock = mock(IConverter.class);
+        final ITokenizer tokenizerMock = mock(ITokenizer.class);
+        final Evaluator evaluator = new Evaluator(converterMock, tokenizerMock);
+        final Operator dummyOperator = makeDummyOperator();
+
+        evaluator.addOperator(dummyOperator);
+
+        verify(converterMock).addOperator(dummyOperator.name, dummyOperator.priority);
+    }
+
+    @Test
+    void positiveTest_AddOperator_VerifyTokenizer() {
+        final IConverter converterMock = mock(IConverter.class);
+        final ITokenizer tokenizerMock = mock(ITokenizer.class);
+        final Evaluator evaluator = new Evaluator(converterMock, tokenizerMock);
+        final Operator dummyOperator = makeDummyOperator();
+
+        evaluator.addOperator(dummyOperator);
+
+        verify(tokenizerMock).addOperator(dummyOperator.name);
+    }
+
+    @Test
+    void positiveTest_NothingAdded_SupportedFunctions() {
         final Evaluator evaluator = new Evaluator();
 
         assertTrue(evaluator.getFunctions().isEmpty());
     }
 
     @Test
-    void noFunctionsNoOperatorsEmptyOperators() {
+    void positiveTest_NothingAdded_SupportedOperators() {
         final Evaluator evaluator = new Evaluator();
 
         assertTrue(evaluator.getOperators().isEmpty());
     }
 
     @Test
-    void noFunctionsNoOperatorsEvaluate() {
+    void positiveTest_NothingAdded_EvaluationResult() {
         final Evaluator evaluator = new Evaluator();
         final String inputExpression = "A B C";
         final String evaluatedExpression = evaluator.evaluate(inputExpression);
 
         assertEquals(inputExpression, evaluatedExpression);
+    }
+
+    private static Function makeDummyFunction() {
+        final String dummyName = "Function";
+        final int dummyNumberOfArguments = 2;
+
+        return new Function(dummyName, dummyNumberOfArguments){
+            protected boolean checkArguments() {
+                return false;
+            }
+            protected Object result() {
+                return null;
+            }
+        };
+    }
+
+    private static Operator makeDummyOperator() {
+        final String dummyName = "Operator";
+        final int dummyPriority = 1;
+        final int dummyNumberOfArguments = 1;
+
+        return new Operator(dummyName, dummyPriority, dummyNumberOfArguments){
+            protected boolean checkArguments() {
+                return false;
+            }
+            protected Object result() {
+                return null;
+            }
+        };
     }
 }
 
