1d76098c30c73e88de4b7d59ff2eb8ca1e17f6e3
Mikhail Zinin <mzinin@gmail.com>
2019-07-13 12:12:32 +0200

    Unit tests for FunctionNow added

---
+++ src/test/java/mzinin/petproject/s2e2/functions/FunctionNowTests.java
@@ -0,0 +1,60 @@
+package mzinin.petproject.s2e2.functions;
+
+import mzinin.petproject.s2e2.TestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+
+class FunctionNowTests {
+
+    @Test
+    void positiveTest_StackSize() {
+        final FunctionNow function = new FunctionNow();
+        final Stack<Object> stack = TestUtils.createStack();
+
+        TestUtils.invoke(function, stack);
+
+        assertEquals(1, stack.size());
+    }
+
+    @Test
+    void positiveTest_ResultType() {
+        final FunctionNow function = new FunctionNow();
+        final Stack<Object> stack = TestUtils.createStack();
+
+        TestUtils.invoke(function, stack);
+
+        assertTrue(stack.peek() instanceof OffsetDateTime);
+    }
+
+    @Test
+    void positiveTest_ResultValue() {
+        final FunctionNow function = new FunctionNow();
+        final Stack<Object> stack = TestUtils.createStack();
+
+        TestUtils.invoke(function, stack);
+
+        final OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
+        final OffsetDateTime functionResult = (OffsetDateTime)stack.peek();
+        final long maxDifferenceInSeconds = 2;
+
+        assertTrue(now.isAfter(functionResult));
+        assertTrue(now.toEpochSecond() - functionResult.toEpochSecond() < maxDifferenceInSeconds);
+    }
+
+    @Test
+    void positiveTest_MoreArguments_StackSize() {
+        final FunctionNow function = new FunctionNow();
+        final Stack<Object> stack = TestUtils.createStack(Boolean.FALSE, "A", "B");
+
+        TestUtils.invoke(function, stack);
+
+        assertEquals(4, stack.size());
+    }
+}
