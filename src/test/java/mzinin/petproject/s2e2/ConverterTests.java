3753feed6faf8e957cd913621516da36d53d4639
Mikhail Zinin <mzinin@gmail.com>
2019-07-15 20:42:47 +0200

    More unit tests for Converter

--- src/test/java/mzinin/petproject/s2e2/ConverterTests.java
+++ src/test/java/mzinin/petproject/s2e2/ConverterTests.java
@@ -1,7 +1,8 @@
 package mzinin.petproject.s2e2;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -11,7 +12,7 @@ import java.util.List;
 class ConverterTests {
 
     @Test
-    void positiveTest_OneBInaryOperator_ResultValue() {
+    void positiveTest_OneBinaryOperator_ResultValue() {
         final Converter converter = new Converter();
         converter.addOperator("+", 1);
 
@@ -89,5 +90,254 @@ class ConverterTests {
                                                          new Token(TokenType.ATOM, "B"),
                                                          new Token(TokenType.OPERATOR, "!="));
         assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_OneFunctionWithoutArguments_ResultValue() {
+        final Converter converter = new Converter();
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_OneFunctionOneArgument_ResultValue() {
+        final Converter converter = new Converter();
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.ATOM, "Arg1"), 
+                                                         new Token(TokenType.FUNCTION, "FUN"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_OneFunctionThreeArguments_ResultValue() {
+        final Converter converter = new Converter();
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.ATOM, "Arg2"),
+                                                      new Token(TokenType.ATOM, "Arg3"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.ATOM, "Arg1"), 
+                                                         new Token(TokenType.ATOM, "Arg2"),
+                                                         new Token(TokenType.ATOM, "Arg3"),
+                                                         new Token(TokenType.FUNCTION, "FUN"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_FunctionAndExernalOperator_ResultValue() {
+        final Converter converter = new Converter();
+        converter.addOperator("+", 1);
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"),
+                                                      new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg2"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.ATOM, "Arg1"), 
+                                                         new Token(TokenType.FUNCTION, "FUN"),
+                                                         new Token(TokenType.ATOM, "Arg2"),
+                                                         new Token(TokenType.FUNCTION, "FUN"),
+                                                         new Token(TokenType.OPERATOR, "+"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_FunctionAndInternalOperator_ResultValue() {
+        final Converter converter = new Converter();
+        converter.addOperator("+", 1);
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.ATOM, "Arg2"),
+                                                      new Token(TokenType.COMMA, ","),
+                                                      new Token(TokenType.ATOM, "Arg3"),
+                                                      new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.ATOM, "Arg4"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.ATOM, "Arg1"), 
+                                                         new Token(TokenType.ATOM, "Arg2"),
+                                                         new Token(TokenType.OPERATOR, "+"),
+                                                         new Token(TokenType.ATOM, "Arg3"), 
+                                                         new Token(TokenType.ATOM, "Arg4"), 
+                                                         new Token(TokenType.OPERATOR, "+"),
+                                                         new Token(TokenType.FUNCTION, "FUN"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_NestedFunctions_ResultValue() {
+        final Converter converter = new Converter();
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN1"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.FUNCTION, "FUN2"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"),
+                                                      new Token(TokenType.COMMA, ","),
+                                                      new Token(TokenType.FUNCTION, "FUN3"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.COMMA, ","),
+                                                      new Token(TokenType.ATOM, "Arg2"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN2"), 
+                                                         new Token(TokenType.ATOM, "Arg1"),
+                                                         new Token(TokenType.ATOM, "Arg2"), 
+                                                         new Token(TokenType.FUNCTION, "FUN3"), 
+                                                         new Token(TokenType.FUNCTION, "FUN1"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_OperatorsWithoutArguments_ResultValue() {
+        final Converter converter = new Converter();
+        converter.addOperator("+", 1);
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.OPERATOR, "+"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.OPERATOR, "+"), 
+                                                         new Token(TokenType.OPERATOR, "+"),
+                                                         new Token(TokenType.OPERATOR, "+"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_FunctionWithoutCommas_ResultValue() {
+        final Converter converter = new Converter();
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.ATOM, "Arg2"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.ATOM, "Arg1"), 
+                                                         new Token(TokenType.ATOM, "Arg2"),
+                                                         new Token(TokenType.FUNCTION, "FUN"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void positiveTest_FunctionOfOperators_ResultValue() {
+        final Converter converter = new Converter();
+        converter.addOperator("+", 1);
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final List<Token> actualTokens = converter.convert(inputTokens);
+
+        final List<Token> expectedTokens = Arrays.asList(new Token(TokenType.OPERATOR, "+"), 
+                                                         new Token(TokenType.OPERATOR, "+"),
+                                                         new Token(TokenType.FUNCTION, "FUN"));
+        assertIterableEquals(expectedTokens, actualTokens);
+    }
+
+    @Test
+    void negativeTest_UnpairedLeftBracket() {
+        final Converter converter = new Converter();
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.LEFT_BRACKET, "("),
+                                                      new Token(TokenType.ATOM, "Arg1"));
+
+        final ExpressionException thrown = assertThrows(
+            ExpressionException.class,
+            () -> converter.convert(inputTokens),
+            "Expected Converter to throw, but it didn't");
+
+        assertTrue(thrown.getMessage().contains("Unpaired bracket"));
+    }
+
+    @Test
+    void negativeTest_UnpairedRightBracket() {
+        final Converter converter = new Converter();
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.FUNCTION, "FUN"),
+                                                      new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.RIGHT_BRACKET, ")"));
+
+        final ExpressionException thrown = assertThrows(
+            ExpressionException.class,
+            () -> converter.convert(inputTokens),
+            "Expected Converter to throw, but it didn't");
+
+        assertTrue(thrown.getMessage().contains("Unpaired bracket"));
+    }
+
+    @Test
+    void negativeTest_TwoOperatorsWithTheSameName() {
+        final Converter converter = new Converter();
+        converter.addOperator("+", 1);
+
+        final ExpressionException thrown = assertThrows(
+            ExpressionException.class,
+            () -> converter.addOperator("+", 1),
+            "Expected Converter to throw, but it didn't");
+
+        assertTrue(thrown.getMessage().contains("Operator + is already added"));
+    }
+
+    @Test
+    void negativeTest_UnknownOperator() {
+        final Converter converter = new Converter();
+        converter.addOperator("+", 1);
+
+        final List<Token> inputTokens = Arrays.asList(new Token(TokenType.ATOM, "Arg1"),
+                                                      new Token(TokenType.OPERATOR, "+"),
+                                                      new Token(TokenType.ATOM, "Arg2"),
+                                                      new Token(TokenType.OPERATOR, "*"),
+                                                      new Token(TokenType.ATOM, "Arg3"));
+
+        final ExpressionException thrown = assertThrows(
+            ExpressionException.class,
+            () -> converter.convert(inputTokens),
+            "Expected Converter to throw, but it didn't");
+
+        assertTrue(thrown.getMessage().contains("Unknown operator *"));
     }
 }
